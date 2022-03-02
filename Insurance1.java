015889bc0d541a0a111ae49b73f5e27093d5af4b
Venkat <Venkat@tcs.com>
2022-03-02 19:18:18 +0530

    FirstFile

---
+++ Insurance1.java
@@ -0,0 +1,20 @@
+public abstract class Insurance implements Print
+
+// class variables
+
+int PolicyNumber;
+double MonthlyPremium;
+
+// public methods
+
+Insurance(int n, double b)
+
+int getPolicyNumber()
+
+double getMonthlyPremium()
+
+double getAnnualPremium()
+
+void setPolicyNumber(int n)
+
+abstract void setMonthlyPremium(double r);
